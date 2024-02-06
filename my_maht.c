c94db1b5a42174829d84fd467c262024f072e2a2
Netanelbruch <netanel0205@gamil.com>
2024-02-06 20:44:19 +0200

    second commit

---
+++ my_maht.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include "my_maht.h"
+
+
+
+int min(int num1, int num2){
+    
+    if (num1<num2)
+    {
+        return num1;
+    }
+    else
+    {
+        return num2;
+    }
+    
+}
+
+
+void A(int arr[N][N])
+{
+
+    for(int i = 0; i<N ; i++)
+    {
+        for (int j = 0; j< N; j++)
+        {
+                
+            scanf("%d", &arr[i][j]);
+        }
+
+    }
+
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (j==i)
+            {
+                continue;
+            }
+            
+           for (int k = 0; k < N; k++)
+           {
+              
+                if(k == i || k==j){
+                    continue;
+                }    
+
+                if ( arr[j][i] == 0 ||  arr[i][k] == 0)
+                {
+                    continue;
+                }
+
+                
+
+                if (arr[j][k] == 0 && j!=k) 
+                {
+                    arr[j][k] = arr[j][i] + arr[i][k];
+                } 
+
+                else{
+                    arr[j][k] = min(arr[j][k], arr[j][i] + arr[i][k]); 
+                }
+                
+                
+           }
+              
+        }
+    }
+
+}
+
+
+
+void B(int matrix[N][N], int i, int j)
+{
+
+     if (matrix[i][j]==0)
+    {
+         printf("False\n");
+    }
+    else
+    {
+       printf("True\n");
+    }
+   
+}
+
+
+
+void C(int matrix[N][N], int i, int j)
+{
+
+    if (matrix[i][j] == 0)
+    {
+        printf("-1\n");
+    }
+    else
+    {
+        printf("%d\n", matrix[i][j]);
+    }
+    
+}
