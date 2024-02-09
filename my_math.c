3216caec2a66c173ddc8dea13479b2777143f08e
Netanelbruch <netanel0205@gamil.com>
2024-02-09 14:06:11 +0200

    update

--- my_math.c
+++ my_math.c
@@ -3,6 +3,19 @@
 
 
 
+// void print(int arr[N][N]){
+//     for(int i = 0; i<N ; i++)
+//         {
+//             for (int j = 0; j< N; j++)
+//             {
+                
+//                 printf("%d ", arr[i][j]);
+//             }
+//             printf("\n");
+//         }
+// }
+
+
 int min(int num1, int num2){
     
     if (num1<num2)
